3a9ed66c291f0f4c6abf6e5b2c3ba3ccca48e77f
ElRojo <diegoabrajan52@aragon.unam.mx>
2024-01-15 19:32:02 -0600

    Replicas updated to use 3 static blocks

--- start.sh
+++ start.sh
@@ -14,11 +14,10 @@ nohup /usr/sbin/sshd 2> /dev/null
 if [ -z "${MASTER_HOSTNAME}" ] && [ "${ISMASTER}" = false ]; then echo -e "${Red}[!] Error${NC}: variable MASTER_HOSTNAME for slave node expected some value"; exit 1; fi
 if [ -z "${SLAVE_HADOOP_PASSWORD}" ] && [ "${ISMASTER}" = true ]; then echo -e "${Red}[!] Error${NC}: variable SLAVE_HADOOP_PASSWORD for master node expected some value"; exit 1; fi
 if [ -z "${MASTER_HADOOP_PASSWORD}" ]; then echo -e "${Yellow}[!] Warning${NC}: variable MASTER_HADOOP_PASSWORD unset; assuming master node"; fi
-if [ "${REPLICAS}" -le 0 ]; then echo -e "${Red}[!] Error${NC}: variable REPLICAS has an unvalid value; please provide a valid value"; exit 1; fi
+if [ "${NODES}" -le 0 ]; then echo -e "${Red}[!] Error${NC}: variable NODES has an unvalid value; please provide a valid value"; exit 1; fi
 if [ -z "${HOSTNAME}" ]; then echo -e "${Yellow}[!] Warning${NC}: variable HOSTNAME unset; using default container hostname: '$(hostname)'"; HOSTNAME=$(hostname); fi
 echo -n "hadoop:${PASSWORD}" | chpasswd &> /dev/null
 
-sed -i "s|__REPLICAS__|${REPLICAS}|g" /opt/hadoop/etc/hadoop/hdfs-site.xml
 echo -e "${Cyan}[+]${NC} Applied replicas to hdfs-site.xml"
 
 if [ -n "${MASTER_HOSTNAME}" ] && [ -n "${MASTER_HADOOP_PASSWORD}" ] && [ "${ISMASTER}" = false ]; then
@@ -74,7 +73,7 @@ HostName ${HOSTNAME}"
     cat /home/hadoop/.ssh/id_ed25519.pub >> /home/hadoop/.ssh/authorized_keys
     echo -e "${Cyan}[+]${NC} Added own id_ed25519.pub (master node) to /home/hadoop/.ssh/authorized_keys"
     echo -e "${Cyan}[+]${NC} Waiting for slaves to raise up..."
-    while [ "${REPLICAS}" -ne "$(wc -l < /opt/hadoop/etc/hadoop/workers)" ]; do sleep 2; done
+    while [ "${NODES}" -ne "$(wc -l < /opt/hadoop/etc/hadoop/workers)" ]; do sleep 2; done
     echo -e "${Cyan}[+]${NC} Slaves raised up. Sending ~/.ssh/config and ~/.ssh/authorized_keys to slaves..."
     {
       while read -r p; do
